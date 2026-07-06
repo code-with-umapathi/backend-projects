b355fa6b9f0e1233d4dc4fdd6c3f92b193748a51
code-with-umapathi <codewithumapathi@gmail.com>
2026-07-06 17:16:12 +0530

    auth middleware to protect the api

---
+++ crud-api-docker/src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+const UnauthorizedError = require("../errors/UnauthorizedError");
+const jwt = require("jsonwebtoken");
+function authMiddleware(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        throw new UnauthorizedError("Authentication required");
+    }
+    const [type, token] = authHeader.split(" ");
+    if (type !== "Bearer") {
+        throw new UnauthorizedError("Invalid authentication scheme");
+    }
+    if (!token) {
+        throw new UnauthorizedError("Authentication token is missing");
+    }
+    let payload;
+    try {
+        payload = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        throw new UnauthorizedError("Invlalid ot expire token");
+    }
+    req.user = {
+        id: payload.sub
+    }
+    next();
+}
+module.exports = authMiddleware;
